a61551671d7dc22cab26f91476d71e9b423ce863
CarpediemOP <deevpatel223@gmail.com>
2026-01-10 10:57:34 +0530

    Security changes

--- employeeui/src/api/axiosclient.js
+++ employeeui/src/api/axiosclient.js
@@ -1,3 +1,5 @@
 import axios from "axios";
-const axiosClient =axios.create({baseURL:"http://localhost:5287/api",Headers:{"Content-Type":"application/json"}});
+const axiosClient =axios.create({
+    baseURL:import.meta.env.VITE_API_BASE_URL,
+    Headers:{"Content-Type":"application/json"}});
 export default axiosClient;
